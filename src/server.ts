3e5063e76558c64e14aef025635c2482dc455bab
RejwanulIslam <174478473+RejwanulIslam@users.noreply.github.com>
2025-12-01 12:10:10 +0600

    server setup and database connect

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from "express"
+import { Pool } from "pg"
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config({ path: path.join(process.cwd(), ".env") })
+const app = express()
+const port = 5000
+
+
+// middle ware
+app.use(express.json())
+
+
+//  DB
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`
+})
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(150) NOT NULL,
+        email VARCHAR(100)UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(20),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+
+  await pool.query(`
+         CREATE TABLE IF NOT EXISTS todos(
+         id SERIAL PRIMARY KEY,
+         user_id INT REFERENCES users(id) ON DELETE CASCADE,
+         title VARCHAR(200) NOT NULL,
+         description TEXT,
+         completed BOOLEAN DEFAULT false,
+         due_date DATE,
+         creacted_at TIMESTAMP DEFAULT NOW(),
+         updated_at TIMESTAMP DEFAULT NOW() 
+
+         ) 
+          `)
+}
+
+initDB()
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
